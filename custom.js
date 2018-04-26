49a259cb34d998617b42b53feb71d317a4bae435
Raziel Fayad <razielfayad@users.noreply.github.com>
2018-04-26 18:45:59 -0500

    indentation

--- custom.js
+++ custom.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-   $("#liAbout").click(function () {
-       document.getElementById("about").scrollIntoView({behavior: "smooth" ,block: "end", inline: "nearest"});
-       $(this).addClass("active");
-       $("#liContact").removeClass("active");
-       $("#liPortfolio").removeClass("active");
-   });
-    $("#liPortfolio").click(function () {
-        document.getElementById("portfolio").scrollIntoView({behavior: "smooth" ,block: "center", inline: "nearest"});
-        $(this).addClass("active");
-        $("#liContact").removeClass("active");
-        $("#liAbout").removeClass("active");
-    });
-    $("#liContact").click(function () {
-        document.getElementById("contact").scrollIntoView({behavior: "smooth" ,block: "start", inline: "nearest"});
-        $(this).addClass("active");
-        $("#liAbout").removeClass("active");
-        $("#liPortfolio").removeClass("active");
-    });
+  $("#liAbout").click(function () {
+    document.getElementById("about").scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"});
+    $(this).addClass("active");
+    $("#liContact").removeClass("active");
+    $("#liPortfolio").removeClass("active");
+  });
+  $("#liPortfolio").click(function () {
+    document.getElementById("portfolio").scrollIntoView({behavior: "smooth", block: "center", inline: "nearest"});
+    $(this).addClass("active");
+    $("#liContact").removeClass("active");
+    $("#liAbout").removeClass("active");
+  });
+  $("#liContact").click(function () {
+    document.getElementById("contact").scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    $(this).addClass("active");
+    $("#liAbout").removeClass("active");
+    $("#liPortfolio").removeClass("active");
+  });
 });
